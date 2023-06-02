1f40d8bd53b8fedcb8c6a05d0d89d15513e8db96
Natalie210985 <bns_natalie@mail.ru>
2023-06-02 13:13:19 +0300

    Homework

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+const getHole = index => document.getElementById(`hole${index}`);
+
+function clear (){
+  dead.textContent = 0;
+  lost.textContent = 0;
+}
+
+for (let i = 1; i <= 9; i++) {
+
+ getHole(i).onclick =  function (){
+
+    if (getHole(i).classList.contains('hole_has-mole')){
+      dead.textContent++;
+    }
+
+    else {
+      lost.textContent++;
+    }
+
+    if (dead.textContent == 10) {
+      alert('Победа!');
+      clear();
+    }
+
+    else if (lost.textContent == 5) {
+      alert('Вы проиграли');
+      clear();
+    }
+  }
+}
