dd46870e9d1e9af7b170d0583bde1b25cf1966d3
Natalie210985 <bns_natalie@mail.ru>
2023-06-04 09:05:14 +0300

    Homework

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,28 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownLink = document.querySelectorAll('.dropdown__link');
+const dropdown = document.querySelectorAll('.dropdown');
+
+
+
+function dropdownClose () { 
+    dropdownValue.addEventListener("click", () =>{
+    dropdownList.classList.toggle('dropdown__list_active');
+    })
+}
+dropdownClose();
+
+function dropdownUp () {
+    dropdownList.classList.toggle('dropdown__list_active');
+}
+
+let linkArray = Array.from(dropdownLink);
+
+linkArray.forEach((el,idx) => {
+
+el.addEventListener('click', (e) =>{
+    e.preventDefault();
+    dropdownValue.textContent = el.textContent;
+    dropdownUp ();
+});
+});
